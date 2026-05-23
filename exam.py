0667036d598e69d29365e6f3648ba09bfa3a180e
arthurfachaux-afk <arthur.fachaux@gmail.com>
2026-05-23 12:58:49 +0000

    rewrite

--- exam.py
+++ exam.py
@@ -1,5 +1,34 @@
-#this is final exam python code.
-#this app is a calculator
+num1=int(input("Enter a first number:"))
+num2=int(input("Enter a second number:"))
+choice=int(input("To choose a type of calculation, type 1 for multiplication, 2 for division, 3 for substraction, and 4 for addition:"))
+match choice:
+    case 1: #multiplication
+        print(num1,"*",num2,"=",num1*num2)
+        question=input("Do you want the multiplication table of the two numbers you chosed ? (Answer by 'yes' or 'no')")
+        if question=="yes":
+            for a in range (21):
+                print(num1,"*",a,"=",num1*a)
+            print()
+            for b in range (21):
+                print(num2,"*",b,"=",num2*b)
+        else:
+            print("Ok enjoy your day.3")
 
-x = 5
-print(x)
+    case 2: #division
+        print(num1,"/",num2,"=",num1/num2)
+        question2=input("Do you want the division table of the two numbers you chosed ? (Answer by 'yes' or 'no')")
+        if question2=="yes":
+            for y in range (21):
+                print(num1,"/",y,"=",num1/y) #handle division by zero
+                print()
+            for z in range (21):
+                print(num2,"/",z,"=",num2/z)
+        else:
+            print("Ok have a good day")
+            
+        
+    case 3: #substraction
+        print(num1,"-",num2,"=",num1-num2)
+
+    case 4: #addition
+        print(num1,"+",num2,"=",num1+num2)
